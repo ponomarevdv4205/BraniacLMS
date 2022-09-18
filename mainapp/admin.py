ea4f7541a120f793ad108f83f0fa1ee462f1dab9
DIMA <ponomarevdv4205@yandex.ru>
2022-09-18 14:38:18 +0300

    lesson_6 Customizing admin panel

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,1 +1,17 @@
-# Register your models here.
+@admin.register(mainapp_models.Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ["id", "get_course_name", "num", "title", "deleted"]
+    ordering = ["-course__name", "-num"]
+    list_per_page = 5
+    list_filter = ["course", "created", "deleted"]
+    actions = ["mark_deleted"]
+
+    def get_course_name(self, obj):
+        return obj.course.name
+
+    get_course_name.short_description = _("Course")
+
+    def mark_deleted(self, request, queryset):
+        queryset.update(deleted=True)
+
+    mark_deleted.short_description = _("Mark deleted")
